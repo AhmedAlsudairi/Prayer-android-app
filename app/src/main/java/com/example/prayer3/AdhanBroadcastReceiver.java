cee937d7e7ea4b3af5c738e528b9c5ea921bfc00
AhmedAlsudairi <AhmedAlsudairi@users.noreply.github.com>
2020-11-19 17:09:02 +0300

    passing the title of prayer to AdhanBroadcastReceiver and AdhanIntentService

--- app/src/main/java/com/example/prayer3/AdhanBroadcastReceiver.java
+++ app/src/main/java/com/example/prayer3/AdhanBroadcastReceiver.java
@@ -15,7 +15,9 @@ public class AdhanBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
 //Builds the notification with all the parameters
+        String title = intent.getStringExtra("title");
         Intent intent1 = new Intent(context, AdhanIntentService.class);
+        intent1.putExtra("title",title);
         context.startService(intent1);
     }
 
--- app/src/main/java/com/example/prayer3/AdhanIntentService.java
+++ app/src/main/java/com/example/prayer3/AdhanIntentService.java
@@ -8,28 +8,28 @@ import android.content.Intent;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
-public class AdhanIntentService extends
-        IntentService {
+public class AdhanIntentService extends IntentService {
     private static final int NOTIFICATION_ID = 3;
 
     public AdhanIntentService() {
-        super("MyNewIntentService");
+        super("AdhanIntentService");
     }
 
     @Override
     protected void onHandleIntent(Intent intent) {
+        String title = intent.getStringExtra("title");
         Intent notifyIntent = new Intent(this, MainActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 2, notifyIntent, PendingIntent.FLAG_UPDATE_CURRENT);
         NotificationCompat.Builder notifyBuilder =
                 new NotificationCompat.Builder(this, "notifyChannel")
                         .setContentTitle("Prayer app")
-                        .setContentText("Adhan Time")
+                        .setContentText(title+"Adhan Time")
                         .setSmallIcon(R.drawable.ic_launcher_background)
                         .setPriority(NotificationCompat.PRIORITY_DEFAULT)
                         .setDefaults(NotificationCompat.DEFAULT_ALL)
                         .setContentIntent(pendingIntent);
         //try
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
-        notificationManager.notify(200,notifyBuilder.build());
+        notificationManager.notify(NOTIFICATION_ID,notifyBuilder.build());
     }
 }
